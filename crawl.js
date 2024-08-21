d6e91f33b0297393f89c760b9a5661b24159c4d0
Aferuza <feruza06.02@gmail.com>
2024-08-20 21:24:04 -0700

    tests url normialize with jest

--- crawl.js
+++ crawl.js
@@ -1,13 +1,22 @@
 function normalizeURL(urlString){
-    //
+    //use built in url constuctor
     const urlObj = new URL(urlString)
 
     //we only care for hostname and pathname
-    return `${urlObj.hostname}${urlObj.pathname}`
+    const hostpath = `${urlObj.hostname}${urlObj.pathname}`
+
+    //if the hostpath isnt empy and the last char is a /
+    if(hostpath.length>0 && hostpath.slice(-1)==='/'){
+        //return evthing untill the last char - slash
+        return hostpath.slice(0,-1);
+    }
+    //otherwise return the hostpath as it is
+    return hostpath;
     }
     
 // makes normalizeURL available to other functions
 module.exports = {
-    normalizeURL}
+    normalizeURL,
+}
 
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,17 +1,36 @@
 const {normalizeURL} = require('./crawl.js');
 //import fns from jest
-
 const{test,expect}= require('@jest/globals');
 
 
-//now write our tests: theres a name, which takes out fn and another fn
+//1. writing our test suite: theres a name, which takes out fn and another fn
+//test #1:
 test('normalizeURL strip protocol', ()=>{
-    //set input to empty for now
+    // put our url into a var input
     const input = 'https://blog.boot.dev/path';
     //set actual var to wht the function with input as param returns
     const actual = normalizeURL(input);
     const expected = 'blog.boot.dev/path';
-
     //I am expecting the actual output to be equal to the expected output- if they equal- log it as a pass test
     expect(actual).toEqual(expected);
+})
+
+//test #2:
+test('normalizeURL strip trailing slash',()=>{
+    const input = 'https://blog.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+
+
+
+})
+
+//test #3:
+test('normalizeURL capitals',()=> {
+    const input = 'https://BLOG.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+
 })
